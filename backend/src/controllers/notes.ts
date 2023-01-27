83666e001a3ec8831df0bfcbcaace2241a03f84e
MM-WebDeveloper <milan.varnamo@gmail.com>
2023-01-28 00:56:40 +0100

    created controllers and express routes for the notes functions create and get all notes

---
+++ backend/src/controllers/notes.ts
@@ -0,0 +1,26 @@
+import { RequestHandler } from 'express';
+import NoteModel from '../models/note';
+
+export const getNotes: RequestHandler = async (req, res, next) => {
+	try {
+		const notes = await NoteModel.find().exec();
+		res.status(200).json(notes);
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const createNote: RequestHandler = async (req, res, next) => {
+	const { title, text } = req.body;
+
+	try {
+		const newNote = await NoteModel.create({
+			title,
+			text,
+		});
+
+		res.status(201).json(newNote);
+	} catch (error) {
+		next(error);
+	}
+};
