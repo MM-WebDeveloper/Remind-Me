3bfc9e6f1bbd2f8b5bd055a994c35df2bee1a7dc
MM-WebDeveloper <milan.varnamo@gmail.com>
2023-02-01 21:58:23 +0100

    feat: :sparkles: Add GET request all notes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,27 @@
+import { useEffect, useState } from 'react';
+import { Note } from './models/note';
+
 const App = () => {
-	return <div>App</div>;
+	const [notes, setNotes] = useState<Note[]>([]);
+
+	useEffect(() => {
+		console.log('happened');
+		const getNotes = async () => {
+			try {
+				const response = await fetch('/api/notes', {
+					method: 'GET',
+				});
+				const notes = await response.json();
+				setNotes(notes);
+				console.log(notes);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+
+		getNotes();
+	}, []);
+
+	return <div>{JSON.stringify(notes)}</div>;
 };
 export default App;
