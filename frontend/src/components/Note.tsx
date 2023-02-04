01f6e22818c68bc494d3c3cb2dc67ac34d8d98da
MM-WebDeveloper <milan.varnamo@gmail.com>
2023-02-04 21:32:57 +0100

    feat: :sparkles: Add date formatter util function

--- frontend/src/components/Note.tsx
+++ frontend/src/components/Note.tsx
@@ -1,5 +1,6 @@
 import styles from '../styles/Note.module.css';
 import { Note as NoteModel } from '../models/note';
+import { formatDate } from '../utils/formatDate';
 
 interface NoteProps {
 	note: NoteModel;
@@ -7,12 +8,20 @@ interface NoteProps {
 
 const Note = ({ note }: NoteProps) => {
 	const { title, text, createdAt, updatedAt } = note;
+
+	let createdUpdatedText: string;
+
+	if (updatedAt > createdAt) {
+		createdUpdatedText = 'Updated: ' + formatDate(updatedAt);
+	} else {
+		createdUpdatedText = 'Created: ' + formatDate(createdAt);
+	}
+
 	return (
 		<div className={styles.card}>
 			<p>Title: {title}</p>
 			<p>Description: {text}</p>
-			<p>Created: {createdAt}</p>
-			<p>Last Updated: {updatedAt}</p>
+			<p>{createdUpdatedText}</p>
 		</div>
 	);
 };
---
+++ frontend/src/utils/formatDate.ts
@@ -0,0 +1,9 @@
+export function formatDate(dateString: string): string {
+	return new Date(dateString).toLocaleDateString('en-GB', {
+		year: 'numeric',
+		month: 'short',
+		day: 'numeric',
+		hour: 'numeric',
+		minute: 'numeric',
+	});
+}
