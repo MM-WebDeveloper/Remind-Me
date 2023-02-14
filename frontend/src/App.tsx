902aa18a1442767bf724db378ea387a586e57597
MM-WebDeveloper <milan.varnamo@gmail.com>
2023-02-14 20:20:52 +0100

    feat: :sparkles: Add spinner and error message if failing to get initial notes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,19 +3,27 @@ import NoteModal from './components/NoteModal';
 import Note from './components/Note';
 import { Note as NoteModel } from './models/note';
 import * as NotesApi from './network/notes.api';
+import Spinner from './components/Spinner';
 
 const App = () => {
 	const [notes, setNotes] = useState<NoteModel[]>([]);
+	const [notesLoading, setNotesLoading] = useState(true);
+	const [notesLoadingError, setNotesLoadingError] = useState(false);
 	const [showNoteModal, setShowNoteModal] = useState(false);
 	const [noteToEdit, setNoteToEdit] = useState<NoteModel | null>(null);
 
 	useEffect(() => {
 		const getNotes = async () => {
 			try {
+				setNotesLoadingError(false);
+				setNotesLoading(true);
 				const notes = await NotesApi.fetchNotes();
 				setNotes(notes);
 			} catch (error) {
 				console.error(error);
+				setNotesLoadingError(true);
+			} finally {
+				setNotesLoading(false);
 			}
 		};
 
@@ -57,16 +65,27 @@ const App = () => {
 	return (
 		<>
 			<button onClick={() => setShowNoteModal(true)}>Add new note</button>
-			<div>
-				{notes.map((note) => (
-					<Note
-						key={note._id}
-						editNote={editNote}
-						note={note}
-						deleteNote={deleteNote}
-					/>
-				))}
-			</div>
+			{notesLoading && <Spinner />}
+			{notesLoadingError && <p>500 Internal Server Error</p>}
+			{!notesLoading && !notesLoadingError && (
+				<>
+					{notes.length > 0 ? (
+						<div>
+							{notes.map((note) => (
+								<Note
+									key={note._id}
+									editNote={editNote}
+									note={note}
+									deleteNote={deleteNote}
+								/>
+							))}
+						</div>
+					) : (
+						<p>Your list is empty.</p>
+					)}
+				</>
+			)}
+
 			{showNoteModal && (
 				<NoteModal
 					noteToEdit={noteToEdit}
