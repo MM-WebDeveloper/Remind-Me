158f35b851cbbc630eabd0822b45cf257d63a627
MM-WebDeveloper <milan.varnamo@gmail.com>
2023-03-03 00:47:20 +0100

    Add authentication protection middleware

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import * as UserController from '../controllers/users';
+import { authMiddleware } from '../middleware/auth';
 
 const router = express.Router();
 
-router.get('/', UserController.getAuthenticatedUser);
+router.get('/', authMiddleware, UserController.getAuthenticatedUser);
 router.post('/register', UserController.Register);
 router.post('/login', UserController.login);
 router.post('/logout', UserController.logout);
